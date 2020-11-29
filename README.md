b6bbb1d585ec990cab3d4d498001e35bc5c5771e
Tony Perriello <tony.perriello@philips.com>
2020-11-29 14:16:40 -0500

    First pass at dockerizing the app! It works!

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # Cookbook
 An MEAN stack app for my spouse and I to keep track of and search for recipes we like.
 
-## To do
-- Add models for Recipes, Ingredients, Units, and Tags
-- Dockerize everything
-
 ## Refactors to consider
 - UI: Consider DRYing out Ingredients/Units/Tags CRUD and List. Keeping them separate for now.
--- api/src/db/mongoose.js
+++ api/src/db/mongoose.js
@@ -3,8 +3,12 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-    process.env.MONGO_URI || 'mongodb://localhost:27017/cookbook', 
+    process.env.MONGO_URI || 'mongodb://localhost:27017/cookbook',
     { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB database connection established successfully!');
+});
 
 module.exports = mongoose;
